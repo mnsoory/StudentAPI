45a0590dcfe164dd8da11a7003534cdb309c09c5
Mohamed Almansoury <mnsory2021@gmail.com>
2025-12-24 23:07:52 +0200

    feat: add Permissions navigation property to User and remove IUserPermissionRepository

--- StudentAPI.Domain/Entities/User.cs
+++ StudentAPI.Domain/Entities/User.cs
@@ -8,5 +8,7 @@ namespace StudentAPI.Domain.Entities
         public string Username { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
+
+        public virtual ICollection<UserPermission> Permissions { get; set; } = new List<UserPermission>();
     }
 }
